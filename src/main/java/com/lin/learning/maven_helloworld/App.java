0b5a5b782a81ba52bf9af98f3180e8f7b6d9943c
Mao Lin <BG352480@800best.net>
2018-07-27 14:45:53 +0800

    提交说明

---
+++ src/main/java/com/lin/learning/maven_helloworld/App.java
@@ -0,0 +1,13 @@
+package com.lin.learning.maven_helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void sayHello()
+    {
+        System.out.println( "Hello World..." );
+    }
+}
